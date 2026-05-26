880ee0b0e25051e7b9d715c3dd2dafba74560c9d
Foezi Arisandi <foeziadahkli@gmail.com>
2026-05-26 19:36:49 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ ini_set('display_errors', 1);
 
 // Mengambil konfigurasi dari Environment Variables di Cloud Run
 $db_host = getenv('DB_HOST') ?: '127.0.0.1';
-$db_user = getenv('DB_USER') ?: 'root';
-$db_pass = getenv('DB_PASS') ?: '';
+$db_user = getenv('DB_USER') ?: 'admin_db_siakad';
+$db_pass = getenv('DB_PASS') ?: 'Rahasia@00';
 $db_name = getenv('DB_NAME') ?: 'db_demo1';
 
 // Jika menggunakan Cloud SQL Auth Proxy, port defaultnya tetap 3306 via localhost
