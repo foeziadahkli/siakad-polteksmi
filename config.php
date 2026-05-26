0025242167c99bcd1b4cadfa85a40909c869a6f9
Foezi Arisandi <foeziadahkli@gmail.com>
2026-05-26 22:17:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,14 +2,22 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Mengambil konfigurasi dari Environment Variables di Cloud Run
-$db_host = getenv('DB_HOST') ?: '127.0.0.1';
 $db_user = getenv('DB_USER') ?: 'admin_db_siakad';
 $db_pass = getenv('DB_PASS') ?: 'Rahasia@00';
 $db_name = getenv('DB_NAME') ?: 'db_demo1';
 
-// Koneksi standar via TCP/IP lokal (Proxy Cloud Run yang akan meneruskannya ke IP Publik secara aman)
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+// Cloud Run secara otomatis menyediakan Unix Socket di folder /cloudsql/
+// jika Cloud SQL Connection sudah Anda aktifkan di menu pengaturan.
+$connection_name = getenv('CLOUD_SQL_CONNECTION_NAME') ?: 'project-09bafd93-b5fb-4cda-b2f:asia-southeast2:dbsiakad'; 
+
+if (getenv('CLOUD_SQL_CONNECTION_NAME') || !empty($connection_name)) {
+    // KONEKSI UNTUK CLOUD RUN (Wajib NULL untuk host dan port, socket di ujung)
+    $conn = mysqli_connect(null, $db_user, $db_pass, $db_name, null, "/cloudsql/$connection_name");
+} else {
+    // KONEKSI UNTUK LOKAL PC
+    $db_host = getenv('DB_HOST') ?: '127.0.0.1';
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+}
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
